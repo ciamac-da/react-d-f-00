e8f7aea6dc1eb68db4de458cbc3428c32238e917
ciamac-da <ciamac.davoudi@gmail.com>
2021-06-19 21:56:28 +0200

    Verified by Marti4n v1.0.1

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import { React } from 'react';
+
+
+const AddUser = (props) => {
+
+    const AddUserHandler = (e) => {
+        e.preventDefault()
+    }
+
+    return ( 
+        <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age(Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+        </form>
+     );
+}
+ 
+export default AddUser;
