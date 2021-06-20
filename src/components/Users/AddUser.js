8099d279688eaad97d82a74c8f74e60d3cd81c2d
ciamac-da <ciamac.davoudi@gmail.com>
2021-06-20 12:27:08 +0200

    Verified by Marti4n v1.0.3 button re-usable  user input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,36 @@
-import { React } from 'react';
+import React, {useState}  from 'react';
 import Card from '../UI/Card';
 import classes from "./AddUser.module.css";
-
+import Button from '../UI/Button';
 
 const AddUser = (props) => {
 
+    const [enteredUsername, setEnteredUsername] = useState("")
+    const [enteredAge     ,      setEnteredAge] = useState("")
+
+
+
     const AddUserHandler = (e) => {
         e.preventDefault()
+        console.log(enteredUsername, enteredAge)
+    }
+
+    const usernameChangeHandler = (e) => {
+        setEnteredUsername(e.target.value)
+    }
+
+    const ageChangeHandler = (e) => {
+        setEnteredAge(e.target.value)
     }
 
     return ( 
         <Card className={classes.input}>
         <form  onSubmit={AddUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-        <label htmlFor="age">Age(Years)</label>
-        <input type="number" id="age" />
-        <button type="submit">Add User</button>
+        <input type="text" id="username" onChange={usernameChangeHandler}/>
+        <label htmlFor="age" >Age(Years)</label>
+        <input type="number" id="age" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
         </form>
         </Card>
      );
